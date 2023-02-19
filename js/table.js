df094e3962e13ec398b306b4701dae005131a8ac
Dennis Mullen <dkmullen@gmail.com>
2023-02-19 13:13:47 -0500

    Add and refine delete action

--- js/table.js
+++ js/table.js
@@ -17,21 +17,26 @@ function makeTable() {
     for (let val in record) {
       row += `<td>${record[val]}</td>`;
     }
-    row += `<td>
+    row += `<td class="edit-cell" id="edit-${record.id}">
     <svg class="edit-icon" width="24" height="24" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M30.1136 1.88638C28.8976 0.668794 27.2784 0 25.5585 0C23.8401 0 22.2209 0.668794 21.0049 1.88638L2.75065 20.139C2.75065 20.139 2.74905 20.1438 2.74585 20.1454C2.61625 20.2766 2.39545 20.6222 2.32825 20.883L0.0482689 30.0093C-0.0893299 30.5533 0.0722687 31.1325 0.469065 31.5277L0.470665 31.5293L0.472265 31.5309C0.869062 31.9277 1.44506 32.0893 1.99065 31.9533L11.117 29.6701C11.3778 29.6029 11.725 29.3822 11.8546 29.2526C11.8562 29.251 11.861 29.2478 11.861 29.2478L30.1152 10.9967C31.3312 9.77912 32.0016 8.16313 32 6.44155C32 4.72156 31.3312 3.10397 30.1152 1.88638H30.1136ZM19.8577 7.56474L24.4385 12.1455L10.7298 25.8542L6.14902 21.2734L19.8577 7.56474ZM3.80024 28.2014L4.75223 24.403L7.6002 27.2526L3.80024 28.2014ZM27.8544 4.15036C29.0768 5.37435 29.0768 7.50874 27.8544 8.73273L26.704 9.88472L22.1185 5.30076L23.2721 4.15036C24.4961 2.92797 26.6304 2.92637 27.8544 4.15036Z"/>
     </svg>
-    </td></tr>`;
+    </td>`;
+    row += `<td class="delete-cell" id="delete-${record.id}">
+    <svg class="delete-icon" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px"><path d="M6 4H18V21H6z" opacity=".3"/><path d="M11 18H9V7h2V18zM15 18h-2V7h2V18zM4 3H20V5H4z"/><path d="M17 5L14 2 10 2 7 5z"/><path d="M17,22H7c-1.1,0-2-0.9-2-2V3h14v17C19,21.1,18.1,22,17,22z M7,5v15h10V5H7z"/></svg>
+
+    </td>
+    </tr>`;
     t.innerHTML += row;
   });
 }
 
 function editRow() {
-  // 'this' is the clicked row
-  let selected = data.find((o) => o.id === parseInt(this.cells[0].innerHTML));
-  console.log(selected);
+  // 'this' is the clicked cell
+  let rowId = this.id.split('-')[1];
+  let selected = data.find((o) => o.id === parseInt(rowId));
   document.querySelector('#editModal').style['display'] = 'block';
-  let modal = document.querySelector('.modal-body');
+  let modal = document.querySelector('.edit-modal-body');
   for (let item in selected) {
     if (item !== 'id') {
       modal.innerHTML += `<div><b>${item.charAt(0).toUpperCase()}${item.slice(
@@ -41,15 +46,37 @@ function editRow() {
   }
 }
 
-function closeModal() {
-  console.log('hey', document.querySelector('#editModal'));
-  document.querySelector('#editModal').style['display'] = 'none';
-  document.querySelector('.modal-body').innerHTML = '';
+function deleteRow() {
+  // 'this' is the clicked cell
+  let rowId = this.id.split('-')[1];
+  let selected = data.find((o) => o.id === parseInt(rowId));
+  document.querySelector('#deleteModal').style['display'] = 'block';
+  let modal = document.querySelector('.delete-modal-body');
+  for (let item in selected) {
+    if (item !== 'id') {
+      modal.innerHTML += `<div><b>${item.charAt(0).toUpperCase()}${item.slice(
+        1
+      )}:</b> ${selected[item]}</div>`;
+    }
+  }
+}
+
+function closeModal(name) {
+  let elem = document.querySelector(name);
+  elem.style['display'] = 'none';
+  elem.querySelector('.modal-body').innerHTML = '';
 }
 
 setTimeout(() => {
   document.querySelectorAll('tr').forEach((row) => {
-    row.addEventListener('click', editRow);
+    row.editIcon = row.querySelector('.edit-cell');
+    if (row.editIcon) {
+      row.editIcon.addEventListener('click', editRow);
+    }
+    row.deleteIcon = row.querySelector('.delete-cell');
+    if (row.deleteIcon) {
+      row.deleteIcon.addEventListener('click', deleteRow);
+    }
   });
 }, 500);
 
